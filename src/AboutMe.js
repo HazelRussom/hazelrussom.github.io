1d75f514c7385120a9d9846cf281e1bd263f790b
Daniel Russom <daniel.russom00@gmail.com>
2023-10-23 08:09:05 +0100

    Update AboutMe text

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -8,10 +8,10 @@ export default function AmoutMe() {
         About Me
       </Typography>
       <Typography color='primary' variant='body1'>
-        I have over 3 years of experience working in FinTech as a Software Engineer. 
+        I have over 5 years of experience working as a Software Engineer. 
         My desire to learn more pushes me to regularly learn new technologies, and given me a comfortable base for full-stack roles.
         I have a passion for planning and optimising tasks to work in the most efficient way I can.
-        Currently I'm working remotely with a full-time position at FNZ.
+        Currently I'm working remotely with a full-time position at Codurance.
       </Typography>
     </Box>
   );
