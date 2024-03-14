d727e6435d5b30d4fcebcce526d246b6aa1cd587
Daniel Russom <daniel.russom00@gmail.com>
2024-03-14 14:22:14 +0000

    Fix name

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <Box m={4}>
       <Typography color='primary' variant='h1' align="center">
-        Daniel Russom
+        Hazel Russom
       </Typography>
       <Typography color='primary' variant='h2' align="center">
         Software Engineer
