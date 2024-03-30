5834ce77337725722836478a396cc8b803640a0c
Daniel Russom <daniel.russom00@gmail.com>
2024-03-30 10:19:50 +0000

    WIP Translations

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Typography, Box } from '@mui/material'
+import { useTranslation } from 'react-i18next';
 
 export default function Header() {
+  const { t } = useTranslation();
   return (
     <Box m={4}>
       <Typography color='primary' variant='h1' align="center">
-        Hazel Russom
+        {t("name")}
       </Typography>
       <Typography color='primary' variant='h2' align="center">
         Software Engineer
