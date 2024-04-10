b1764a6c57cc5e128cc1b097de3f4ecb81b792c3
Daniel Russom <daniel.russom00@gmail.com>
2024-04-10 10:37:55 +0100

    Fix spacing on links

--- src/Links.js
+++ src/Links.js
@@ -5,10 +5,10 @@ import LinkCard from "./LinkCard"
 export default function Links() {
   return (
     <Grid container spacing={4} justify="center" >
-      <Grid item xs={3}>
+      <Grid item xs={6}>
         <LinkCard title="LinkedIn" link="https://www.linkedin.com/in/daniel-russom/" image="LinkedIn.png"/>
       </Grid>
-      <Grid item xs={3}>
+      <Grid item xs={6}>
         <LinkCard title="GitHub" link="https://github.com/DanielRussom/" image="Github.png"/>
       </Grid>
     </Grid>
