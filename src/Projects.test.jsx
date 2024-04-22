272dbddd9061abad9e559e726d52a8bb9ace6507
Daniel Russom <daniel.russom00@gmail.com>
2024-04-22 12:09:01 +0100

    Init Projects .test.jsx

---
+++ src/Projects.test.jsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import { <Projects></Projects> } from "./<Projects></Projects>";
+
+describe('<Projects></Projects> should', () => {
+  it('Do a thing', () => {
+    render(<<Projects></Projects>/>);
+
+    //Assert
+  });
+});
