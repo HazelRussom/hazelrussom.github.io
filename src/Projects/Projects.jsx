bfa352726cd9f4e591b94fbf6ebbfb77598d5ed3
Daniel Russom <daniel.russom00@gmail.com>
2024-05-04 20:24:48 +0100

    Get from Reader

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -1,4 +1,9 @@
+import { ProjectReader } from "./ProjectReader"
+
 export default function Projects() { 
+    const reader = new ProjectReader();
+    reader.get();
+
     return (
         <p>Projects</p>
     )
