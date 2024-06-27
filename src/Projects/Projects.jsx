26e298cc37895e8d0243cee763e3da8d5f0ea8c8
Daniel Russom <daniel.russom00@gmail.com>
2024-06-27 14:24:07 +0100

    Fix failing test

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -4,17 +4,13 @@ import ProjectCard from "./ProjectCard";
 export default function Projects() { 
     const reader = new ProjectReader();
     const projects = reader.get();
-
-    console.warn(projects.length);
-    if(projects.length > 0){
-        console.warn(projects[0].name)
-    }
+   
     return (
         <>
             <p>Projects</p>
             { projects.length > 0 && 
-            <p>Aaa</p> &&
-            <ProjectCard project={projects[0]}/>
+            
+             <ProjectCard project={projects[0]}/>
             }
         </>
         
---
+++ src/Projects/Projects.test.tsx
@@ -0,0 +1,62 @@
+import { render, screen } from "@testing-library/react";
+import Project from "./Project";
+import Projects from "./Projects";
+import ProjectReader from "./ProjectReader";
+
+vi.mock("./ProjectReader");
+
+vi.mock("./ProjectCard", () => {
+  return {
+    default: ({ project }: { project: Project }) => (
+      <span data-testid="project">{project.name}</span>
+    ),
+  };
+});
+
+describe("Projects should", () => {
+  beforeEach(() => {
+    ProjectReader.prototype.get = vi.fn().mockImplementation(() => {
+      return [];
+    });
+  });
+
+  it("render a title", () => {
+    render(<Projects />);
+
+    expect(screen.getByText("Projects")).toBeVisible();
+  });
+
+  it("get all projects", () => {
+    let getProjects = vi.fn();
+    ProjectReader.prototype.get = getProjects;
+
+    getProjects.mockImplementation(() => {
+      return [];
+    });
+
+    render(<Projects />);
+
+    expect(getProjects).toBeCalledTimes(1);
+  });
+
+  it("render no projects", () => {
+    render(<Projects />);
+
+    expect(screen.queryByTestId("project")).not.toBeInTheDocument();
+  });
+
+  it("render one project", () => {
+    let getProjects = vi.fn();
+    const projects = [new Project("Test")];
+
+    getProjects.mockImplementation(() => {
+      return projects;
+    });
+
+    ProjectReader.prototype.get = getProjects;
+
+    render(<Projects />);
+
+    expect(screen.getByTestId("project")).toHaveTextContent("Test");
+  });
+});
